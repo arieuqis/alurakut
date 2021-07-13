fcb5f78095f6abbf1acb532e703fb54130d8bba8
arieuqis <siqueira.bcl@gmail.com>
2021-07-13 11:34:57 -0300

    Inicio alurakut

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,59 @@
-import styled from 'styled-components'
+import Box from "../src/components/Box";
+import MainGrid from "../src/components/MainGrid";
+import { ProfileRelationsBoxWrapper } from "../src/components/ProfileRelations";
+import { AlurakutMenu, OrkutNostalgicIconSet } from "../src/lib/AlurakutCommons";
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+function ProfileSidebar({ gitHubUser }) {
+  return (
+    <Box>
+      <img src={`https://github.com/${gitHubUser}.png`} />
+    </Box>
+  );
+}
 
 export default function Home() {
-  return <Title>My page</Title>
+  const gitHubUser = "arieuqis";
+  const pessoasFavoritas = ["juunegreiros", "peas", "omariosouto"];
+
+  return (
+    <>
+      <AlurakutMenu />
+
+      <MainGrid>
+        <div className="profileArea">
+          <ProfileSidebar gitHubUser={gitHubUser} />
+        </div>
+        <div className="welcomeArea">
+          <Box>
+            <h1 className="title">
+
+            Bem vindo
+            </h1>
+
+            <OrkutNostalgicIconSet/>
+            </Box>
+        </div>
+        <div className="profileRelationsArea">
+          <ProfileRelationsBoxWrapper>
+            <h2 className="smallTitle">
+              Pessoas da comunidade ({pessoasFavoritas.length})
+            </h2>
+
+            <ul>
+              {pessoasFavoritas.map((itemAtual) => {
+                return (
+                  <li>
+                    <a href={`/users/${itemAtual}`} key={itemAtual}>
+                      <img src={`https://github.com/${itemAtual}.png`} />
+                      <span>{itemAtual}</span>
+                    </a>
+                  </li>
+                );
+              })}
+            </ul>
+          </ProfileRelationsBoxWrapper>
+        </div>
+      </MainGrid>
+    </>
+  );
 }
